63cf4c89ad608611a6bc5d12f40c86f1e4308160
rosie <rosiemcgrath@yahoo.com>
2026-06-01 20:44:07 +0100

    updating db connection

--- php/dbconn.php
+++ php/dbconn.php
@@ -1,19 +1,19 @@
 <?php
-    $pass = "FCqcx0T11jRTJqn3";
-    $user = "rmcgrath13";
-    $webserver = "rmcgrath13.lampt.eeecs.qub.ac.uk";
-    $db = "rmcgrath13";
-
+    $pass = $_ENV['DB_PASS'] ?? getenv('DB_PASS');
+    $user = $_ENV['DB_USER'] ?? getenv('DB_USER');
+    $db = $_ENV['DB_NAME'] ?? getenv('DB_NAME');
+    
     $conn = new mysqli($webserver, $user, $pass, $db);
     
 
+    // For Cloud SQL, use Unix socket or IP
+    $webserver = "/cloudsql/" . $_ENV['CLOUD_SQL_CONNECTION_NAME'];  // Unix socket (preferred)
+    // OR use the Cloud SQL IP directly
+    // $webserver = "1.2.3.4";  // Get IP from Cloud SQL instance
+    
+    $conn = new mysqli($webserver, $user, $pass, $db);
+    
     if(!$conn){
         echo $conn -> error;
-    } 
-
-        
-   // if ($conn -> connect_error) 
-      //  {echo "there has been an error ".$conn->connect_error;}
-      
-      
-      ?>
+    }
+?>
